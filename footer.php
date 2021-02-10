304124ba394977b0d31687ce222e5f14a6aa06dc
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-10 13:56:20 +0100

    Update mobile

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
         <footer>
             <div class="container">
             <div class="row">
-            <div class="col">
+            <div class="col-md-4">
                 <h4>Quick links</h4>
                 <?php wp_nav_menu( ['theme_location' => 'footer-menu'] ); ?>
             </div>
-            <div class="col">
+            <div class="col-md-4">
                 <h4>Kontact</h4>
                 <ul class="contact">
                     <li><i class="fas fa-phone-alt"></i> 0351 8489549</li>
@@ -13,7 +13,7 @@
                     <li><i class="far fa-clock"></i> Mon - Frei: 09:00 - 17:00 </li>
                 </ul>
             </div>
-            <div class="col">
+            <div class="col-md-4">
             </div>
             </div>
             </div>
