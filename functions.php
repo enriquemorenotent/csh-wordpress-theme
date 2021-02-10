8356bb340030c01e4d206e093e60e2e3460725f4
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-10 11:25:29 +0100

    many changes

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 include "functions/set-features-support.php";
 include "functions/enqueue-styles-and-scripts.php";
 include "functions/register-menus.php";
@@ -9,3 +13,4 @@ include "functions/set-custom-taxonomies.php";
 include "functions/add-shortcodes.php";
 include "functions/woocommerce.php";
 include "functions/gutenberg.php";
+
