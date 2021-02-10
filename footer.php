659efb3d2e5bf9c6ed240039a3ae16baf8ce1814
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-10 12:10:56 +0100

    Updated footer

--- footer.php
+++ footer.php
@@ -2,28 +2,18 @@
             <div class="container">
             <div class="row">
             <div class="col">
+                <h4>Quick links</h4>
                 <?php wp_nav_menu( ['theme_location' => 'footer-menu'] ); ?>
             </div>
             <div class="col">
-                <h4>Contact info</h4>
-                <ul>
-                    <li>+49 XXXX XXXXXXXX</li>
-                    <li>XXXX@XXXXX.de</li>
-                    <li>
-                        Montag - Freitag<br/>
-                        09:00 - 17:00
-                    </li>
+                <h4>Kontact</h4>
+                <ul class="contact">
+                    <li><i class="fas fa-phone-alt"></i> 0351 8489549</li>
+                    <li><i class="far fa-envelope"></i> cosmetic_dd@web.de</li>
+                    <li><i class="far fa-clock"></i> Mon - Frei: 09:00 - 17:00 </li>
                 </ul>
             </div>
             <div class="col">
-                <h4>Quick links</h4>
-                <ul>
-                    <li><a href="/">Links</a></li>
-                    <li><a href="/">Links</a></li>
-                    <li><a href="/">Links</a></li>
-                    <li><a href="/">Links</a></li>
-                    <li><a href="/">Links</a></li>
-                </ul>
             </div>
             </div>
             </div>
